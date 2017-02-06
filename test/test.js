f0b46f045d148218e4309e9b025c739b5d59bca9
Shariati, Amin <amin.shariati@sc.com>
2017-02-06 10:54:09 +0800

    Added basic test file.

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+/* eslint-env mocha */
+var assert = require('assert');
+var systeminfo = require('../');
+
+console.log(systeminfo());
+describe('systeminfo', function () {
+  it('should return System info', function (done) {
+    done();
+  });
+});
